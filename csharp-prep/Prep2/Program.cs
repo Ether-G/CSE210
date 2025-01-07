441a45a951d9fec5d5b197bf8398eefb4c37b10e
Ben Black <toastedben@gmail.com>
2025-01-06 18:11:38 -0700

    Completed Prep 2 & Stretch

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Decided to use ternary operators instead od a linear if else chain to make code more compact.
+        
+        // Prompting the user for input of grade percentage
+        Console.Write("Enter your grade percentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        // Determination of the letter grade
+        string letter = percentage >= 90 ? "A" :
+                        percentage >= 80 ? "B" :
+                        percentage >= 70 ? "C" :
+                        percentage >= 60 ? "D" : "F";
+
+        string sign = (letter != "A" && letter != "F") ? 
+                      (percentage % 10 >= 7 ? "+" : (percentage % 10 < 3 ? "-" : "")) : 
+                      (letter == "A" && percentage < 93 ? "-" : "");
+
+        Console.WriteLine($"Your final grade is: {letter}{sign}");
+
+        // pass/fail message
+        Console.WriteLine(percentage >= 70 
+                          ? "Congratulations! You passed the course." 
+                          : "Don't give up! Better luck next time.");
     }
 }
